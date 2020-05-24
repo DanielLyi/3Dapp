a472565d1942141bb9a8506259f6782b0f44219a
Danylo <danylo.liakh@gmail.com>
2020-05-24 14:24:44 +0300

    Added the equals method to Point3 class to properly check equality.
    This method enables proper checking of the equal points in PointSet class.

--- src/Point3.java
+++ src/Point3.java
@@ -1,4 +1,10 @@
+/**
+ * A class that represents a vector of a
+ * form (x, y, z), where x, y and z are
+ * in a subset of real numbers.
+ */
 public class Point3 {
+    /*Cartesian coordinates of the point*/
     private double x;
     private double y;
     private double z;
@@ -21,9 +27,39 @@ public class Point3 {
         return z;
     }
 
+    /**
+     * Checks if a specified object equals to this one.
+     * @param obj an object to compare with
+     * @return true if and only if obj
+     * is Point and the corresponding coordinates
+     * are equal
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Point3)) {
+            return false;
+        }
+
+        Point3 p3 = (Point3) obj;
+        return p3.x == x && p3.y == y &&
+                p3.z == z;
+
+    }
+
+    /**
+     * Returns a String
+     * representation of the vector
+     * in the form (x, y, z).
+     */
     @Override
     public String toString() {
-        return "<" + x + ", " + y
-                + ", " + z + ">";
+        return "(" + x + ", " + y
+                + ", " + z + ")";
     }
 }
